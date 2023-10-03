06910a710402c72040125eda5deecdb6ba10a0d1
Débora Mendes de Azevedo <86240362+deboraMendesAzevedo@users.noreply.github.com>
2023-10-03 17:00:13 -0300

    inclusão l

--- Code/app_laravel/resources/views/app/fornecedor/index.blade.php
+++ Code/app_laravel/resources/views/app/fornecedor/index.blade.php
@@ -1,33 +1,5 @@
-<h3>Fornecedor</h3>
-
-
-{{-- para fazer comentário --}}
-
-
-{{ 'teste'}}
-
-<?= 'teste '?>
-
-
-@php
-
-    // Para comentários de uma linha
-    /*
-        Para comentários de multiplas linhas
-    */
-    
+<h3>Fornecedor teste</h3>
 
 
 
 
-
-@endphp
-
-@if(count($fornecedores) > 0 && count($fornecedores)<10)
-    <h3>Existe fornecedores cadastrados</h3>
-@elseif(count($fornecedores) > 10)
-    <h3>existem  vários fornecedores</h3>
-@else
-    <h3>Não existe fornecedor </h3>
-
-@endif
