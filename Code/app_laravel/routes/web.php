9584f5550048e898b0e46a6e6ed1da21e7756c67
Débora Mendes de Azevedo <86240362+deboraMendesAzevedo@users.noreply.github.com>
2023-10-04 17:54:28 -0300

    inclusao de dados

--- Code/app_laravel/routes/web.php
+++ Code/app_laravel/routes/web.php
@@ -11,26 +11,31 @@ Route::get('/request', function(){ // teste para começar novamente
     return 'x';
 });
 
-Route::get('/',[\App\Http\Controllers\PrincipalController::class,'principal']);
+Route::get('/',[\App\Http\Controllers\PrincipalController::class,'principal'])->name('site.index');
 
-Route::get('/contato',[\App\Http\Controllers\ContatoController::class,'contato']);
+Route::get('/contato',[\App\Http\Controllers\ContatoController::class,'contato'])->name('site.contato');
 
-Route::get('/sobre-nos',[\App\Http\Controllers\SobreNosController::class,'sobreNos']);
+Route::get('/sobre-nos',[\App\Http\Controllers\SobreNosController::class,'sobreNos'])->name('site.sobrenos');
 
 Route::get('/login', function(){
     return 'Login';
-});
+})->name('site.login');
 
+Route::prefix('/app')->group(function(){
+
+    //para fazer grupos 
 Route::get('/clientes', function(){
     return 'Clientes';
-});
+})->name('app.clientes');
 
 Route::get('/fornecedores', function(){
     return 'Fornecedores';
-});
+})->name('app.fornecedores');
 
 Route::get('/produtos', function(){
     return 'Produtos';
+})->name('app.produtos');
+
 });
 
 /*
