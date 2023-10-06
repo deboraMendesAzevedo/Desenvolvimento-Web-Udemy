01e36084266df1c85b9b306033c0c6a479618619
Débora Mendes de Azevedo <86240362+deboraMendesAzevedo@users.noreply.github.com>
2023-10-06 13:25:20 -0300

    inclusão de dados

--- Code/app_laravel/resources/views/app/fornecedor/index.blade.php
+++ Code/app_laravel/resources/views/app/fornecedor/index.blade.php
@@ -152,7 +152,7 @@ teste quando o valor não for informado
 @endisset
 
 <br>
-usando o for
+<h2>usando o for</h2>
 <br>
 
 @for($i = 0; $i < 10; $i++)
@@ -192,3 +192,41 @@ usando o for
     @endfor
 
 @endisset
+
+<h2>Usando o while</h2>
+
+@isset($fornecedores) {{-- Para verificar se a variavel existe --}}
+    <br>
+
+    @php $i = 0  @endphp
+    @while (isset($fornecedores[$i]))
+
+    Fornecedor : {{ $fornecedores [$i]['nome'] }}
+    <br>
+    Status : {{ $fornecedores[$i]['status'] }}
+    <br>
+    CNPJ : {{ $fornecedores[$i]['cnpj'] ?? ''}}    {{-- ?? é para testar a variável de existe e se o valor não é null --}}
+    <br>
+    Telefone : {{ $fornecedores[$i]['ddd'] ?? ''}} {{ $fornecedores[$i]['telefone'] ?? ''}}
+    <br>
+    @switch($fornecedores[$i]['ddd'])
+        @case('11')
+            São Paulo = SP
+            @break
+        @case('85')
+            Fortaleza - CE
+            @break
+        @case('32')
+            Juiz de Fora - MG
+        @break
+        @default
+            Estado não informado
+
+    @endswitch
+    <br>
+    <hr>
+    @php $i ++  @endphp
+    @endwhile
+
+@endisset
+
