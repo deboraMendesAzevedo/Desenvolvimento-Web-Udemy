f8a02bb8fc8d7045c05a58e595958320236109f7
Débora Mendes de Azevedo <86240362+deboraMendesAzevedo@users.noreply.github.com>
2023-10-10 14:51:23 -0300

    inclusão de dados do mysql

--- Code/app_laravel/database/migrations/2023_10_10_171330_alter_fornecedores_novas_colunas.php
+++ Code/app_laravel/database/migrations/2023_10_10_171330_alter_fornecedores_novas_colunas.php
@@ -11,10 +11,10 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('fornecedores', function (Blueprint $table) {
-            $table->id();
-            $table->string('nome',50);
-            $table->timestamps();
+        Schema::table('fornecedores', function (Blueprint $table) {
+            $table->string('uf', 2);
+            $table->string('email', 150);
+
         });
     }
 
@@ -23,6 +23,9 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('fornecedores');
+        Schema::table('fornecedores', function (Blueprint $table) {
+            $table->dropColumn(['uf', 'email']);
+
+        });
     }
 };
