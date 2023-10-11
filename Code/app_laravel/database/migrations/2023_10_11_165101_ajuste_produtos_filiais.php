38c2418d14c0e75b7ea9995e03a79b7c555affd7
Débora Mendes de Azevedo <86240362+deboraMendesAzevedo@users.noreply.github.com>
2023-10-11 15:54:36 -0300

    inclusão de dados

--- Code/app_laravel/database/migrations/2023_10_11_165101_ajuste_produtos_filiais.php
+++ Code/app_laravel/database/migrations/2023_10_11_165101_ajuste_produtos_filiais.php
@@ -11,14 +11,50 @@ return new class extends Migration
      */
     public function up(): void
     {
-        //
-    }
+            //criando a tabela filiais
+            Schema::create('filiais', function (Blueprint $table) {
+                $table->id();
+                $table->string('filial', 30);
+                $table->timestamps();
+            });
 
-    /**
-     * Reverse the migrations.
-     */
-    public function down(): void
-    {
-        //
+            //criando a tabela produto_filiais
+            Schema::create('produto_filiais', function (Blueprint $table) {
+                $table->id();
+                $table->unsignedBigInteger('filial_id');
+                $table->unsignedBigInteger('produto_id');
+                $table->decimal('preco_venda', 8, 2);
+                $table->integer('estoque_minimo');
+                $table->integer('estoque_maximo');
+                $table->timestamps();
+
+                //foreign key (constraints)
+                $table->foreign('filial_id')->references('id')->on('filiais');
+                $table->foreign('produto_id')->references('id')->on('produtos');
+            });
+
+            //removendo colunas da tabela produtos
+            Schema::table('produtos', function (Blueprint $table) {
+                $table->dropColumn(['preco_venda', 'estoque_minimo', 'estoque_maximo']);
+            });
+        }
+
+        /**
+         * Reverse the migrations.
+         *
+         * @return void
+         */
+        public function down()
+        {
+            //adicionar colunas da tabela produtos
+            Schema::table('produtos', function (Blueprint $table) {
+                $table->decimal('preco_venda', 8, 2);
+                $table->integer('estoque_minimo');
+                $table->integer('estoque_maximo');
+            });
+
+            Schema::dropIfExists('produto_filiais');
+
+            Schema::dropIfExists('filiais');
     }
 };
