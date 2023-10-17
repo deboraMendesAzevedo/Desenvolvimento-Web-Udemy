672b9a268d6b343000a7baf408902e01c375e9cb
Débora Mendes de Azevedo <86240362+deboraMendesAzevedo@users.noreply.github.com>
2023-10-17 14:51:00 -0300

    inclusão de dados

--- Code/app_laravel/database/migrations/2023_10_17_165207_alter_fornecedores_softdelete.php
+++ Code/app_laravel/database/migrations/2023_10_17_165207_alter_fornecedores_softdelete.php
@@ -11,11 +11,9 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('fornecedores', function (Blueprint $table) {
-            $table->id();
-            $table->string('nome',50);
-            $table->timestamps();
-            //$table->softDeletes();
+        Schema::table('fornecedores', function (Blueprint $table) {
+            $table->softDeletes();
+
 
         });
     }
@@ -25,6 +23,9 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('fornecedores');
+        Schema::table('fornecedores', function (Blueprint $table) {
+            $table->dropSoftDeletes();
+
+        });
     }
 };
