3b782382a0006be47fef470e35943dbfd0529288
Débora Mendes de Azevedo <86240362+deboraMendesAzevedo@users.noreply.github.com>
2023-10-17 16:30:08 -0300

    inclusão de dados

--- Code/app_laravel/database/seeders/DatabaseSeeder.php
+++ Code/app_laravel/database/seeders/DatabaseSeeder.php
@@ -4,12 +4,17 @@ namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Str;
 
 class DatabaseSeeder extends Seeder
 {
     /**
      * Seed the application's database.
      */
+
     public function run(): void
     {
         // \App\Models\User::factory(10)->create();
@@ -18,5 +23,6 @@ class DatabaseSeeder extends Seeder
         //     'name' => 'Test User',
         //     'email' => 'test@example.com',
         // ]);
+        $this->call([FornecedorSeeder::class,]);
     }
 }
--- Code/app_laravel/database/seeders/FornecedorSeeder.php
+++ Code/app_laravel/database/seeders/FornecedorSeeder.php
@@ -4,6 +4,7 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use App\Models\Fornecedor;
 
 class FornecedorSeeder extends Seeder
 {
@@ -12,6 +13,31 @@ class FornecedorSeeder extends Seeder
      */
     public function run(): void
     {
-        //
+        // instanciar o objeto
+        $fornecedor = new Fornecedor();
+        $fornecedor->nome = Str('Fornecedor100');
+        $fornecedor->site = Str('fornecedor100.com.br');
+        $fornecedor->uf = Str('CE');
+        $fornecedor->email = Str('contato@fornecedor100.com.br');
+        $fornecedor->save();
+
+        // Usando o create (atenção para o atributo fillable da classe se não tiver precisa incluir)
+        Fornecedor::create([
+            'nome'=> Str('Fornecedor200'),
+            'site'=> Str('fornecedor200.com.br'),
+            'uf'=> Str('RS'),
+            'email'=> Str('contato@fornecedor200.com.br'),
+        ]);
+        //insert por um array
+        DB:table('fornecedores')->insert([
+
+            'nome'=> Str('Fornecedor200'),
+            'site'=>  Str('fornecedor300.com.br'),
+            'uf'=> Str('SP'),
+            'email'=> Str('contato@fornecedor300.com.br'),
+
+        ]);
+
+
     }
 }
