f0107fc6e060c84108a2a830ceaf460cd7880dc4
Débora Mendes de Azevedo <86240362+deboraMendesAzevedo@users.noreply.github.com>
2023-10-23 13:37:34 -0300

    laravel10

---
+++ Code/banco_de_dados/app/Http/Controllers/Teste2/Teste2Controller.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Http\Controllers\Teste2;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class \Teste2Controller extends Controller
+{
+    public function teste2()
+    {
+        return view('admin/supports/index')
+    }
+}
--- Code/banco_de_dados/routes/web.php
+++ Code/banco_de_dados/routes/web.php
@@ -2,6 +2,7 @@
 
 use App\Http\Controllers\site\SiteController;
 use App\Http\Controllers\Admin\SupportController;
+use App\Http\Controllers\Teste2\Teste2Controller;
 use App\Http\Controllers\TesteController;
 use Illuminate\Support\Facades\Route;
 
@@ -17,10 +18,15 @@ use Illuminate\Support\Facades\Route;
 */
 Route::get('/supports', [SupportController::class, 'index'])->name('supports.index');
 
+Route::get('/contato', [Teste2Controller::class, 'contact']);
+
 Route::get('/contato', [SiteController::class, 'contact']);
 
-Route::get('/teste'[TesteController::class,'teste']);
+Route::get('/teste',[TesteController::class,'teste']);
+
+Route::get('teste2', [Teste2Controller::class, 'teste2'] );
 
 Route::get('/', function () {
     return view('welcome');
 });
+
