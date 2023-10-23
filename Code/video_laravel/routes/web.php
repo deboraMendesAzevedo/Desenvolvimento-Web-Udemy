f9ab1a29fbea38faa883a2406b4f78e32688cf52
Débora Mendes de Azevedo <86240362+deboraMendesAzevedo@users.noreply.github.com>
2023-10-23 16:58:24 -0300

    .

--- Code/video_laravel/routes/web.php
+++ Code/video_laravel/routes/web.php
@@ -17,10 +17,6 @@ use Illuminate\Support\Facades\Route;
 
 
 
-//Route::get('/contato', [SiteController::class, 'contact']);
-
-//Route::get('/contato', [SiteController::class, 'contact']);
-
 Route::get('/contato', [SiteController::class, 'contact']);
 
 Route::get('/teste', [TesteController::class, 'teste']);
