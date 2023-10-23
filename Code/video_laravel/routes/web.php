02d01477478c44c21208cbc0bd4f822f341533e9
Débora Mendes de Azevedo <86240362+deboraMendesAzevedo@users.noreply.github.com>
2023-10-23 16:02:51 -0300

    inclusão de dados

--- Code/video_laravel/routes/web.php
+++ Code/video_laravel/routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\Site\SiteController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -13,6 +14,11 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+
+
+Route::get('/contato', [SiteController::class, 'contact']);
+
 Route::get('/', function () {
     return view('welcome');
 });
+
