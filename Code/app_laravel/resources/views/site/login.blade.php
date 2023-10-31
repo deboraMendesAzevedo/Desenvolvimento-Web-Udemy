e316c66c7fd733651f510353b75fc435ac4fb89e
Débora Mendes de Azevedo <86240362+deboraMendesAzevedo@users.noreply.github.com>
2023-10-31 17:27:03 -0300

    inclusão de dados

--- Code/app_laravel/resources/views/site/login.blade.php
+++ Code/app_laravel/resources/views/site/login.blade.php
@@ -8,17 +8,19 @@
             <h1>Formulário </h1>
         </div>
 
-        <div class="informacao-pagina">
-            <div style="width: 30%; margin-left: auto; margin-right: auto;">
-                <form action={{ route('site.login') }} method="post">
-                    @csrf
-                    <input name="usuario"  type= "text" value="{{ old('usuario') }}" placeholder="Usuário" class="borda-preta">
-                    <input name="senha "  type= "password" value="{{ old('senha') }}" placeholder="Senha" class="borda-preta">
-                    <button type="submit"   class="borda-preta"> Acessar</button>
+        <div class="informacao-pagina"><div style="width:30%; margin-left: auto; margin-right: auto;">
+            <form action={{ route('site.login') }} method="post">
+                @csrf
+                <input name="usuario" value="{{ old('usuario') }}" type="text" placeholder="Usuário" class="borda-preta">
+                {{ $errors->has('usuario') ? $errors->first('usuario') : '' }}
 
-                </form>
+                <input name="senha" value="{{ old('senha') }}" type="password" placeholder="Senha" class="borda-preta">
+                {{ $errors->has('senha') ? $errors->first('senha') : '' }}
 
-            </div>
+                <button type="submit" class="borda-preta">Acessar</button>
+            </form>
+            {{ isset($erro) && $erro != '' ? $erro : '' }}
+        </div>
         </div>
         </div>
     </div>
