8bd30b32111a308e1c1d4848098f1655a6e547c9
Débora Mendes de Azevedo <86240362+deboraMendesAzevedo@users.noreply.github.com>
2023-11-07 14:59:05 -0300

    inclusão de dados do fornecedor adicionar

--- Code/app_laravel/app/Http/Controllers/FornecedorController.php
+++ Code/app_laravel/app/Http/Controllers/FornecedorController.php
@@ -3,11 +3,50 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Fornecedor;
 
 class FornecedorController extends Controller
 {
 
     public function index() {
         return view('app.fornecedor.index');
+    }
+
+
+    public function listar(){
+        return view('app.fornecedor.listar');
+    }
+
+    public function adicionar(Request $request){
+
+        if($request->input('_token') != ''){
+            // validação de dados
+
+            $msg = '';
+
+            $regras =[
+                'nome' => 'required|min:3|max:40',
+                'site'=> 'required',
+                'uf' => 'required|min:2|max:2',
+                'email'=> 'email'
+            ];
+
+            $feedback =[
+                'nome' => 'O campo deve ser preenchido',
+                'nome.min' => 'O campo nome  deve ter no mínimo 3 caracteres',
+                'nome.max' => 'O campo nome deve ter no máximo 40 caracteres',
+                'uf.min' => 'O campo uf deve ter no mínimo 2 caracteres',
+                'uf.max' => 'O campo uf deve ter no máximo 2 caracteres',
+                'email.email' => 'O campo e-mail não foi preenchido corretamente'
+            ];
+
+            $request->validate($regras, $feedback);
+
+            $fornecedor = new Fornecedor();
+            $fornecedor->create($request->all());
+
+            $msg = 'Cadastro realizado com sucesso!';
+        }
+        return view('app.fornecedor.adicionar', ['msg' => $msg]);
     }
 }
--- Code/app_laravel/routes/web.php
+++ Code/app_laravel/routes/web.php
@@ -56,6 +56,9 @@ Route::middleware('autenticacao:padrao,visitante')
         Route::get('/sair', [LoginController::class, 'sair'])->name('app.sair');
 
         Route::get('/fornecedor', [FornecedorController::class,'index'])->name('app.fornecedor');
+        Route::post('/fornecedor/listar', [FornecedorController::class,'listar'])->name('app.fornecedor.listar');
+        Route::get('/fornecedor/adicionar', [FornecedorController::class,'adicionar'])->name('app.fornecedor.adicionar');
+        Route::post('/fornecedor/adicionar', [FornecedorController::class,'adicionar'])->name('app.fornecedor.adicionar');
 
         Route::get('/produto', [ProdutoController::class,'index'])->name('app.produto');
 
