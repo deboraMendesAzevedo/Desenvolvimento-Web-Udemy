55e77b16afa65f9dd8cf04bfe247622a04135294
Débora Mendes de Azevedo <86240362+deboraMendesAzevedo@users.noreply.github.com>
2023-11-08 16:27:02 -0300

    até aqui ok

--- Code/app_laravel/app/Http/Controllers/ProdutoController.php
+++ Code/app_laravel/app/Http/Controllers/ProdutoController.php
@@ -33,7 +33,11 @@ class ProdutoController extends Controller
      */
     public function store(Request $request)
     {
-        //
+
+        
+
+        Produto::create($request->all());
+        return redirect()->route('produto.index');
     }
 
     /**
