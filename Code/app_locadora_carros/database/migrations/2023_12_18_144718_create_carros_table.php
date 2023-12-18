b88b22e753074da48078a0def0563a2c202dd493
Débora Mendes de Azevedo <86240362+deboraMendesAzevedo@users.noreply.github.com>
2023-12-18 12:34:33 -0300

    inclusão de dados na tabela

--- Code/app_locadora_carros/database/migrations/2023_12_18_144718_create_carros_table.php
+++ Code/app_locadora_carros/database/migrations/2023_12_18_144718_create_carros_table.php
@@ -13,7 +13,15 @@ return new class extends Migration
     {
         Schema::create('carros', function (Blueprint $table) {
             $table->id();
+            $table->unsignedBigInteger('modelo_id');
+            $table->string('placa', 10)->unique();
+            $table->boolean('disponivel');
+            $table->integer('km');
             $table->timestamps();
+
+            // foreing key
+            $table->foreign('modelo_id')->references('id')->on('modelos');
+
         });
     }
 
--- Code/app_locadora_carros/database/migrations/2023_12_18_145249_create_locacaos_table.php
+++ Code/app_locadora_carros/database/migrations/2023_12_18_145249_create_locacaos_table.php
@@ -11,9 +11,24 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('locacaos', function (Blueprint $table) {
+        Schema::create('locacoes', function (Blueprint $table) {
             $table->id();
+            $table->unsignedBigInteger('cliente_id');
+            $table->unsignedBigInteger('carro_id');
+            $table->dateTime('data_periodo_inicial');
+            $table->dateTime('data_final_previsto_periodo');
+            $table->dateTime('data_final_realizado_periodo');
+            $table->float('valor_diaria', 8,2);
+            $table->integer('km_inicial');
+            $table->integer('km_final');
             $table->timestamps();
+
+
+            // foreing key
+
+            $table->foreing('cliente_id')->references('id')->on('clientes');
+            $table->foreing('carro_id')->references('id')->on('carros');
+
         });
     }
 
@@ -22,6 +37,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('locacaos');
+        Schema::dropIfExists('locacoes');
     }
 };
